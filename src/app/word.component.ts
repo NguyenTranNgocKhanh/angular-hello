7ac5f97d0dfbe92de213fe96315b9d0471fecbb9
Nguyen Tran Ngoc Khanh <ngtrngockhanh2197@gmail.com>
2020-03-09 16:45:34 +0700

    add color

--- src/app/word.component.ts
+++ src/app/word.component.ts
@@ -1,8 +1,14 @@
 import {Component} from '@angular/core';
+import { StylesCompileDependency } from '@angular/compiler';
 
 @Component({
     template: '<h3>This is component word</h3>',
-    selector: 'app-word'
+    selector: 'app-word',
+    styles : [`
+        h3 {
+            coler: red;
+        }
+    `]
 })
 
 export class WordComponent {}
